1d9712f7d6bd51ee6e4c1d2aad1222d83cda6480
peterelauda <peterenlauda02@gmail.com>
2025-04-10 13:30:46 +0800

    echo "# Im-Rich" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/peterelauda/Im-Rich.git git push -u origin main

---
+++ Im Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Im Rich
+//
+//  Created by Peter Enlarga Lauda on 31/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
